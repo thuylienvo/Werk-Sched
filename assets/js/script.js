292aa04356b6cefdd6053c04a2e36d1b0ccb28df
Mimi Vo <mimzy414@gmail.com>
2021-04-24 16:56:00 -0500

    added pastime function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,45 @@
-// WORK SCHEDULE LOGIC 
-
-//SCHEDULE LOGIC 
-
-// VARIABLES
-let timeEL = document.querySelector('.time-block')
-let currentDayEl = document.querySelector('#currentDay')
-
-
-//When I load the page
-//
-currentDayEl.textContent = moment().format('LLLL');
-console.log(currentDayEl);
-
-//Saving a task
-function saveTask() {
-    console.log(this)
+// =========================== FUNCTIONS ========================= //
+//CONVERT STRING TO TIME 
+function timeToSeconds(time) {
+    //console.log(time);
+    time = time.split(/:/);
+    return time[0] * 3600 + time[1] * 60;
 };
 
-document.querySelector('.saveBtn').addEventListener('click', saveTask);
+let currentTime = new Date().getHours() * 3600 +  new Date().getMinutes() * 60;
+console.log(currentTime);
+
+//PAST TIME BLOCK
+function pastTime(timeEL, currentTime) {
+    if (timeEL <= currentTime) {
+        console.log('this is past time');
+    }
+
+}
+
+//PRESENT TIME BLOCK
+
+//FUTURE TIME BLOCK
+
+//SAVE TASK 
+
+//DELETE TASK
+
+
+
+$(document).ready(function(){
+    //console.log('Hello! Y you no worky');
+    
+    //VARIABLES 
+    let currentBlock = $(".time")[0].innerText;
+    let timeEL = $('.time-block');
+    let currentDayEl = $('#currentDay');
+    let timeBlock = timeToSeconds(currentBlock);
+
+    //TODAY'S DATE 
+    currentDayEl.text(moment().format('LLLL'));
+        console.log(currentDayEl);
+
+
+        pastTime(timeBlock, currentTime);    
+})
