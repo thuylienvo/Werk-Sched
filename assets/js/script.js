3b1f2fe56206479855154776829add626608c0ca
Mimi Vo <mimzy414@gmail.com>
2021-04-24 18:48:21 -0500

    added future time function

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,20 +10,33 @@ let currentTime = new Date().getHours() * 3600 +  new Date().getMinutes() * 60;
 console.log(currentTime);
 
 //PAST TIME BLOCK
-function pastTime(timeEL, currentTime) {
+function pastTime(timeEL, currentTime, currentBlock) {
     if (timeEL <= currentTime) {
+        document.querySelector('.task-input')
         console.log('this is past time');
+        currentBlock.parentNode.style.backgroundColor='grey';
     }
-
 }
 
 //PRESENT TIME BLOCK
+function presentTime(timeEL, currentTime, currentBlock) {
+    if (timeEL === currentTime) {
+        document.querySelector('.task-input')
+        console.log('this is future time');
+        currentBlock.parentNode.style.backgroundColor='yellow';
+    }
+}
 
 //FUTURE TIME BLOCK
+function futureTime(timeEL, currentTime, currentBlock) {
+    if (timeEL >= currentTime) {
+        document.querySelector('.task-input')
+        console.log('this is future time');
+        currentBlock.parentNode.style.backgroundColor='green';
+    }
+}
 
 //SAVE TASK 
-
-//DELETE TASK
 
 
 
@@ -31,15 +44,20 @@ $(document).ready(function(){
     //console.log('Hello! Y you no worky');
     
     //VARIABLES 
-    let currentBlock = $(".time")[0].innerText;
+    let currentBlock = $(".time");
     let timeEL = $('.time-block');
     let currentDayEl = $('#currentDay');
-    let timeBlock = timeToSeconds(currentBlock);
+
+    for (let i = 0; i < currentBlock.length; i++) {
+        console.log(i);
+        console.log(currentBlock.length);
+        let timeBlock = timeToSeconds(currentBlock[i].innerText);
+
+        pastTime(timeBlock, currentTime, currentBlock[i]);
+        futureTime(timeBlock, currentTime, currentBlock[i]);
+    }
 
     //TODAY'S DATE 
     currentDayEl.text(moment().format('LLLL'));
         console.log(currentDayEl);
-
-
-        pastTime(timeBlock, currentTime);    
 })
