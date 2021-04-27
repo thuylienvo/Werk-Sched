cb19e911f8b9c87cdce98c572cf1f08789f2a549
Mimi Vo <mimzy414@gmail.com>
2021-04-26 23:38:19 -0500

    setItem

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ function timeToSeconds(time) {
     //console.log(time);
     time = time.split(/:/);
     return time[0] * 3600 + time[1] * 60;
-};
+}
 
 
 let currentTime = new Date().getHours() * 3600 +  new Date().getMinutes() * 60;
@@ -13,51 +13,58 @@ console.log(currentTime);
 
 function validateTime(timeEL, currentTime, currentBlock) {
     if (timeEL < currentTime) {
-        document.querySelector('.task-input')
-        console.log('this is past time');
-        currentBlock.parentNode.style.backgroundColor='lightgrey';
+        document.querySelector(".task-input")
+        console.log("this is past time");
+        currentBlock.parentNode.style.backgroundColor="lightgrey";
     } else if ((timeEL /3600)== new Date().getHours()) {
-        document.querySelector('.task-input')
+        document.querySelector(".task-input")
         console.log('this is present time');
-        currentBlock.parentNode.style.backgroundColor='red';
+        currentBlock.parentNode.style.backgroundColor="red";
     } else if (timeEL > currentTime) {
         document.querySelector('.task-input')
         console.log('this is future time');
-        currentBlock.parentNode.style.backgroundColor='green';
+        currentBlock.parentNode.style.backgroundColor="green";
         
     }
-};
+}
 
 
 //SAVE TASK 
 function saveStorage(button) {
     let userInput = $(button).siblings(".task-input").val();
-    var localStorageKey = $(this).siblings("#task-input").text();
+    var localStorageKey = $(button).siblings("#task-input").attr("id");
+  
     localStorage.setItem(localStorageKey, userInput);
     console.log("saveStorage");
-    console.log(userInput);
-  }
+    //console.log(localStorageKey);
+}
+ 
+function getStorage() {}
 
-
-$(document).ready(function(){
-    
-    //VARIABLES 
-    let currentBlock = $(".hour");
-    let timeEL = $('.hour-block');
-    let currentDayEl = $('#currentDay');
-    let hourEl = $('tr');
+    $(document).ready(function () {
+        //VARIABLES 
+        let currentBlock = $(".hour");
+        let timeEL = $(".hour-block");
+        let currentDayEl = $("#currentDay");
+        let hourEl = $("tr");
+        let textArea;
+        let localStorageKey;
 
     for (let i = 0; i < currentBlock.length; i++) {
         // console.log(i);
         // console.log(currentBlock.length);
         let timeBlock = timeToSeconds(currentBlock[i].innerText);
-
-
+    
         validateTime(timeBlock, currentTime, currentBlock[i]);
-        saveStorage();
+ 
+    //STORE DATA
+        localStorageKey = "task" + (i + 1);
+        textArea = $(currentBlock[i]).siblings(".task-input");
+        textArea.text(localStorage.getItem(localStorageKey));
+        console.log(localStorageKey);
     }
 
     //TODAY'S DATE 
     currentDayEl.text(moment().format('LLL'));
-        console.log(currentDayEl);
-})
+        //console.log(currentDayEl);
+});
